cc474a0aede9613c8e403f79981d37b49e71c1cd
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-18 18:51:52 +0900

    add format
    format

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ function getOutput(){
 	return document.getElementById("output-value").innerText;
 }
 
-
 function printOutput(num){
-	document.getElementById("output-value").innerText=num;
+	document.getElementById("output-value").innerText=getFormattedNumber(num);
 }
+function getFormattedNumber(num){
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
 printOutput("123123");
