ecd473a3a28f921647f7c0b7ebd6b7a6b3e4fe76
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-19 23:45:05 +0900

    add operator
    operatoe clicked

--- script.js
+++ script.js
@@ -10,12 +10,27 @@ function getOutput(){
 }
 
 function printOutput(num){
-	document.getElementById("output-value").innerText=getFormattedNumber(num);
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}
 }
+
 function getFormattedNumber(num){
     var n = Number(num);
     var value = n.toLocaleString("en");
     return value;
 }
 
-printOutput("123123");
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+
+var operator = document.getElementsByClassName("operator");
+for(var i = 0; i<operator.length; i++){
+	operator[i].addEventListener('click', function(){
+		alert("The operator clocked:"+this.id);
+	})
+}
